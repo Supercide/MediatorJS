3b5bdea415dfa0a17cc7408c2a24e689ccad4446
Jordan anderson <jordan_anders@live.co.uk>
2017-10-01 17:50:29 +0100

    Makes messages immutable

--- src/Mediator.ts
+++ src/Mediator.ts
@@ -10,6 +10,10 @@ export interface IMessageHandlerBase {
 export class Dictionary<TKey, TValue> {
     private _internalState: {} = {};
 
+    containsKey(key: TKey): any {
+        return this._internalState.hasOwnProperty(`${key}`);
+    }
+
     add(key: TKey, value: TValue) {
         this._internalState[`${key}`] = value;
     }
@@ -22,28 +26,37 @@ export class Dictionary<TKey, TValue> {
 // tslint:disable-next-line:max-classes-per-file
 export class Mediator {
 
-    private _handlers: Dictionary<string, IMessageHandlerFactoryBase>;
+    private _handlers: Dictionary<string, IMessageHandlerFactoryBase[]>;
 
     constructor() {
-        this._handlers = new Dictionary<string, IMessageHandlerFactoryBase>();    
+        this._handlers = new Dictionary<string, IMessageHandlerFactoryBase[]>();    
     }
 
     public publish<T extends Message>(message: T): void {
 
         const type = this.getMessageType(message);
 
-        const factory = this._handlers.get(type);
+        const factories = this._handlers.get(type);
 
-        const handler = factory.create();
-
-        handler.handle(message);
+        factories.forEach((factory) => {
+            const handler = factory.create();
+            
+            handler.handle(JSON.parse(JSON.stringify(message)));
+        });
     }
 
     public registerHandler<T extends Message>(message: T, handlerFactory: IMessageHandlerFactory<T>) {
         // throw if hash already exists
         const messageType = this.getMessageType(message);
+        
+        if (this._handlers.containsKey(messageType)) {
+            const factories = this._handlers.get(messageType);
+            factories.push(handlerFactory);
+        } else {
+            this._handlers.add(messageType, [handlerFactory]);
+        }
 
-        this._handlers.add(messageType, handlerFactory);
+        
     }
 
     private getMessageType<T extends Message>(message: T): string {
--- tests/Mediator.tests.ts
+++ tests/Mediator.tests.ts
@@ -14,8 +14,19 @@ class TestMessage extends Message {
 class TestMessageHandler implements IMessageHandler<TestMessage> {
     static messages: TestMessage[];
 
-    handle(message: TestMessage) {
-        TestMessageHandler.messages.push(message);
+    handle(testMessage: TestMessage) {
+        TestMessageHandler.messages.push(testMessage);
+    }
+}
+
+// tslint:disable-next-line:max-classes-per-file
+class TestModifyMessageHandler implements IMessageHandler<TestMessage> {
+    static messages: TestMessage[];
+    static modifyMessage: string = 'GoodBye';
+
+    handle(testMessage: TestMessage) {
+        testMessage.message = TestModifyMessageHandler.modifyMessage;
+        TestModifyMessageHandler.messages.push(testMessage);
     }
 }
 
@@ -24,15 +35,17 @@ describe('Mediator Tests', () => {
     let mediator: Mediator;
 
     beforeEach(() => {
-        mediator = new Mediator();
-        mediator.registerHandler(new TestMessage(), { create: () => new TestMessageHandler() });
+        mediator = new Mediator();       
         TestMessageHandler.messages = [];
+        TestModifyMessageHandler.messages = [];
     });
     
     afterEach(() => {
     });
 
     it('GivenTestHandler_WhenPublishingMessage_ThenCallsHandler_WithMessage', () => {
+        mediator.registerHandler(new TestMessage(), { create: () => new TestMessageHandler() });
+
         const testMessage = new TestMessage();
         testMessage.message = 'Hello world';
         mediator.publish(testMessage);
@@ -40,9 +53,34 @@ describe('Mediator Tests', () => {
     });
 
     it('GivenTestHandler_WhenPublishingMessage_ThenCallsHandlerOnce', () => {
+        mediator.registerHandler(new TestMessage(), { create: () => new TestMessageHandler() });
+        
         const testMessage = new TestMessage();
         testMessage.message = 'Hello world';
         mediator.publish(testMessage);
         expect(TestMessageHandler.messages.length).to.equal(1);
+    });
+
+    it('GivenMultipleTestHandlers_WhenPublishingMessage_ThenCallsAllHandlers', () => {
+        mediator.registerHandler(new TestMessage(), { create: () => new TestMessageHandler() });
+        mediator.registerHandler(new TestMessage(), { create: () => new TestMessageHandler() });
+        
+        const testMessage = new TestMessage();
+        testMessage.message = 'Hello world';
+        mediator.publish(testMessage);
+        expect(TestMessageHandler.messages.length).to.equal(2);
+    });
+
+    it('GivenMultipleTestHandlers_WhenPublishingMessage_AndFirstHandlerModifiesMessage_ThenOriginalMessageIsPassedToSecondHandler', () => {
+        mediator.registerHandler(new TestMessage(), { create: () => new TestModifyMessageHandler() });
+        mediator.registerHandler(new TestMessage(), { create: () => new TestMessageHandler() });
+        const expectedMessage = 'Hello world';
+        const expectedModifiedMessage = 'Goodbye';
+        const testMessage = new TestMessage();
+        testMessage.message = 'Hello world';
+        mediator.publish(testMessage);
+
+        expect(TestMessageHandler.messages[0].message).to.equal(expectedMessage);
+        expect(TestModifyMessageHandler.messages[0].message).to.equal(TestModifyMessageHandler.modifyMessage);
     });
 });
